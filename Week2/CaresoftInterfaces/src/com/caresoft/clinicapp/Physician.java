5cc49aff9ccf71083052c3ff876856cc7661de2a
fdrive422 <fafrago@gmail.com>
2022-04-16 16:39:55 -0700

    inital push CareSoft

---
+++ Week2/CaresoftInterfaces/src/com/caresoft/clinicapp/Physician.java
@@ -0,0 +1,59 @@
+package com.caresoft.clinicapp;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Physician extends User implements HIPAACompliantUser {
+    
+    // Inside class:    
+    private ArrayList<String> patientNotes;
+	
+    // Constructor that takes an ID
+    public Physician(Integer id) {
+    	super(id);
+//    	this.patientNotes = patientNotes;
+    }
+    
+    // Implement HIPAACompliantUser!	
+    public void newPatientNotes(String notes, String patientName, Date date) {
+    	String report = String.format(
+    			"Datetime Submitted: %s \n", date);
+    	report += String.format("Reported By ID: %s\n", this.id);
+    	report += String.format("Patient Name: %s\n", patientName);
+    	report += String.format("Notes: %s \n", notes);
+    	this.patientNotes.add(report);
+    }
+
+    @Override
+    public boolean assignPin(int pin) {
+    	// Auto-generated method stub
+    	if(String.valueOf(pin).length()==4) {
+    		this.pin =pin;
+    		return true;
+    	}
+    	else {
+    		return false;
+    	}
+    }
+    
+    @Override
+    public boolean accessAuthorized(Integer confirmedAuthID) {
+    	// TODO Auto-generated method stub
+    	if(this.id == confirmedAuthID) {
+    		return true;
+    	}
+    	else {
+    		return false;    		
+    	}
+    }
+        
+
+	// Setters & Getters
+	public ArrayList<String> getPatientNotes() {
+		return patientNotes;
+	}
+	
+	public void setPatientNotes(ArrayList<String> patientNotes) {
+		this.patientNotes = patientNotes;
+	}
+}
